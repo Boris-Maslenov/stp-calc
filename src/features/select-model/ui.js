7bbfc6c2f953ef8be111d34c901373c5dcba49b2
Boris-Maslenov <ya.maslenov@ya.ru>
2023-02-18 21:50:18 +0300

    FIX: selectedItem

--- src/features/select-model/ui.js
+++ src/features/select-model/ui.js
@@ -5,9 +5,6 @@ import { selectModel } from '../../entities/step-process/';
 export const SelectModel = () => {
     const changeModel = model => dispatch( selectModel(model) );
     const dispatch = useDispatch();
-    const { models } = useSelector(state => state.stepReducer);
-    return <Autocomplete customKey={'model'} items={models} onChange={ (selection) => changeModel(selection) } itemToString={(item) => (item ? item.model : '')}/>
-    
-    
-    //<Autocomplete onChange={model => changeModel(model)} options={ models || [] } label="Выберите модель:" getOptionLabel = { (option) => option.model } />  
+    const { models, model } = useSelector(state => state.stepReducer);
+    return <Autocomplete selectedItem={model} customKey={'model'} items={models} onChange={ (selection) => changeModel(selection) } itemToString={(item) => (item ? item.model : '')}/>
 }
